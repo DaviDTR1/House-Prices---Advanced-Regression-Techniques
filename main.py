f7506630cbdad644cdf6780b5240c9f565f23626
DaviDTR1 <davidluffy01@gmail.com>
2024-04-10 00:35:07 -0400

    One Hot Encoder

--- main.py
+++ main.py
@@ -13,29 +13,39 @@ def best_model(model , X_train, X_val, y_train, y_val):
 
 #Categorical Values
 #Ordinal Encoding
-def ordinal_encoding(X):
-    i = (X.dtypes == 'object')
-    objects_col = list(i[i].index)
+def ordinal_encoding(X_train, X_valid):
+    objects_col = [col for col in X_train.columns if X_train[col].dtype == 'object']
+    good_col = [col for col in objects_col if set(X_valid[col]).issubset(X_train[col])]
+    bad_col = list(set(objects_col) - set(good_col))
     
+    new_X_train = X_train.drop(bad_col, axis=1)
+    new_X_valid = X_valid.drop(bad_col, axis=1)
     ordinal_enc = OrdinalEncoder()
     
-    X[objects_col] = ordinal_enc.fit_transform(X[objects_col])
-    return X[objects_col]
+    new_X_train[good_col] = ordinal_enc.fit_transform(new_X_train[good_col])
+    new_X_valid[good_col] = ordinal_enc.fit_transform(new_X_valid[good_col])
+    return new_X_train, new_X_valid
 
 #One-Hot Encoding
-def oneHot_encoding(X):
-    i = (X.dtypes == 'object')
-    objects_col = list(i[i].index)
+def oneHot_encoding(X_train, X_valid):
+    objects_col = [col for col in X_train.columns if X_train[col].dtype == 'object']
+    low_cardinal_cols = [col for col in objects_col if X_train[col].nunique() < 10]
+    high_cadinal_cols = list(set(objects_col) - set(low_cardinal_cols))
     
     oh_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
-    new_X = pd.DataFrame(oh_encoder.fit_transform(X[objects_col]))
-    new_X.index = X.index
+    oh_X_t = pd.DataFrame(oh_encoder.fit_transform(X_train[low_cardinal_cols]))
+    oh_X_v = pd.DataFrame(oh_encoder.transform(X_valid[low_cardinal_cols]))
+    oh_X_t.index = X_train.index
+    oh_X_v.index = X_valid.index
     
-    X = X.drop(objects_col, axis=1)
-    X = pd.concat([X, new_X], axis=1)
+    X_train = X_train.drop(objects_col, axis=1)
+    X_valid = X_valid.drop(objects_col, axis=1)
+    X_train = pd.concat([X_train, oh_X_t], axis=1)
+    X_valid = pd.concat([X_valid, oh_X_v], axis=1)
     
-    X.columns = X.columns.astype('str')
-    return X    
+    X_train.columns = X_train.columns.astype(str)
+    X_valid.columns = X_valid.columns.astype(str)
+    return X_train, X_valid   
 
 #drop column
 def drop_object(X):
@@ -84,25 +94,27 @@ X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
 y = X_full.SalePrice
 X_full.drop(['SalePrice'],axis=1,inplace=True)
 
-# #only use numerical predictors
-# X = ordinal_encoding(X_full)
-# X_test = ordinal_encoding(X_test_full)
+X, X_test = drop_columns_missing_values(X_full, X_test_full)
 
-X = drop_object(X_full)
-X_test = drop_object(X_test_full)
-
-
-#select the features for the model
-# y = X_full.SalePrice
-# features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
-# X = X_full[features].copy()
-# X_test = X_test_full[features].copy()
+# # drop dtype = 'object'  
+# X = drop_object(X_full)
+# X_test = drop_object(X_test_full)
 
 #split the train data for a better validation
 X_train, X_val, y_train, y_val = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state = 0)
 
+#deal with missing values
+# X_train, X_val = drop_columns_missing_values(X_train=X_train, X_val=X_val)
+# X_train, X_val = imputation(X_train=X_train, X_val=X_val)
+# X_train, X_val = extend_imputation(X_train=X_train, X_val=X_val)
+
+#only use numerical predictors
+# X_train, X_val = ordinal_encoding(X_train, X_val)
+X_train, X_val = oneHot_encoding(X_train, X_val)
+
 #columns with missing values
-missing_val_count = (X_train.isnull().sum())
+# missing_val_count = (X_train.isnull().sum())
+# print(missing_val_count)
 
 #create many model and chose the best
 model1 = RandomForestRegressor(n_estimators= 50, random_state=0)
@@ -116,11 +128,6 @@ models = [model1, model2, model3, model4, model5]
 best = -1
 model = None
 
-#deal with missing values
-# X_train, X_val = drop_columns_missing_values(X_train=X_train, X_val=X_val)
-X_train, X_val = imputation(X_train=X_train, X_val=X_val)
-# X_train, X_val = extend_imputation(X_train=X_train, X_val=X_val)
-
 #iterarte for the models and compare the mean to take the best
 for m in models:
     temp = best_model(m, X_train, X_val, y_train, y_val)
@@ -129,6 +136,10 @@ for m in models:
         best = temp
         model = m
 
+# # ordinal encode training
+# X, X_test = ordinal_encoding(X, X_test)
+# OneHotEncode
+X, X_test = oneHot_encoding(X,X_test)
 
 
 #train the model
@@ -146,4 +157,4 @@ predictions = model.predict(finall_X_test)
 output = pd.DataFrame({'Id': X_test.index,
                        'SalePrice': predictions
 })
-output.to_csv('submission_drop.csv', index=False)
+output.to_csv('submission_oneHot_encode.csv', index=False)
